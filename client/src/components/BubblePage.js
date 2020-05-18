209e292313c83eb61edcc2c38d0fe70dae13bb28
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-17 19:44:56 -0500

    set up fetchColor with axiosWithAuth

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useRouteMatch } from 'react-router-dom';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const match = useRouteMatch();
+
+  const fetchColor = () => {
+    axiosWithAuth()
+    .get("/colors")
+    .then(res => {
+      console.log(res);
+      setColorList(res.data)
+    })
+    .catch(res => {
+      console.log(err);
+    });
+  };
 
   return (
     <>
